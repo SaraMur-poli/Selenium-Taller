5fa4c469b82bdcd94b0d456719e6e5cad3de53b7
Sara M <sara_murillo82221@elpoli.edu.co>
2024-12-16 18:14:22 -0500

    Fligth

--- index.py
+++ index.py
@@ -10,14 +10,13 @@ service = Service("driver/chromedriver.exe")
 bot = webdriver.Chrome(service = service)
 bot.maximize_window()
 time.sleep(1)
-bot.get("https://www.google.com/aclk?sa=l&ai=DChcSEwiHyfqGkKWKAxW4ploFHb3nKnEYABAAGgJ2dQ&co=1&ase=2&gclid=EAIaIQobChMIh8n6hpCligMVuKZaBR295ypxEAAYASAAEgLHF_D_BwE&sig=AOD64_08T32SS9CJ43d0AP7Yjm0IurV8Ig&q&nis=4&adurl&ved=2ahUKEwjXkPWGkKWKAxXFs4QIHZjjFWQQ0Qx6BAgLEAE")
-#bot.get("https://www.viajesexito.com/")
-time.sleep(5)
-'''
-popup = bot.find_element(By.XPATH, '/html/body/div/div/div/div[1]')
+bot.get("https://www.viajesexito.com/")
+time.sleep(10)
+
+popup = bot.find_element(By.XPATH, '/html/body/div/div/div/div[1]/svg/path')
 time.sleep(10)
 popup.click()
-time.sleep(10) '''
+time.sleep(10)
 
 #vuelo + hotel
 fligth = bot.find_element(By.XPATH, '/html/body/form/div[3]/div/div[2]/article/div/div[1]/div/div[1]/div/div/div[2]/div[1]/ul/li[3]/a')
@@ -25,13 +24,3 @@ time.sleep(3)
 fligth.click()
 time.sleep(5)
 
-#buscar aero
-input = "José María Cordova (MDE)"
-airport = bot.find_element(By.XPATH, '/html/body/form/div[3]/div/div[2]/article/div/div[1]/div/div[1]/div/div/div[2]/div[2]/div[3]/div[2]/div[1]/div/div/div[1]/div/div[1]/div/div/input')
-time.sleep(3)
-airport.click()
-time.sleep(5)
-airport.send_keys(input)
-time.sleep(5)
-airport.send_keys(Keys.ENTER)
-time.sleep(5)
