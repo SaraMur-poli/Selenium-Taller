6eef1571741404ec1ab77505499a0735abfc55b4
Sara M <sara_murillo82221@elpoli.edu.co>
2024-12-17 13:23:25 -0500

    Driver settings
    Adding service and driver

--- bot.py
+++ bot.py
@@ -8,3 +8,18 @@ from selenium.webdriver.chrome.options import Options
 from selenium.webdriver.support.ui import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
 from selenium.webdriver.common.action_chains import ActionChains
+
+chrome_options = Options()
+chrome_options.add_argument("--start-maximized")  
+chrome_options.add_argument("-5-incognito")
+chrome_options.add_argument("--ignore-certificate-errors")  
+chrome_options.add_argument("--disable-logging")  
+chrome_options.add_argument("--log-level=3")
+
+service = Service("driver/chromedriver.exe")
+driver = webdriver.Chrome(service = service, options = chrome_options)
+
+url = "https://www.viajesexito.com"
+driver.get(url)
+
+print(f"Open page: {driver.title}")
